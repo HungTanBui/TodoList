9ca7c101b8557336d91bbef194c7e7230d04c3ed
HungTanBui <staboyvn12@gmail.com>
2025-05-20 21:10:47 +0700

    update HTML and JS

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-console.log("Hello Word!");
-document.addEventListener("DOMContentLoaded", getTodos());
+const todoInput = document.getElementById("todo-input");
+const addButton = document.getElementById("add-button");
 
+document.addEventListener("DOMContentLoaded", getTodos());
+addButton.addEventListener("click",addTodo);
 // function getTodos(){
 
 //     // Lấy dữ liệu từ API thông qua fetch
@@ -14,6 +16,7 @@ document.addEventListener("DOMContentLoaded", getTodos());
 // }
 
 // Hàm bất đồng bộ
+// GET FUNCTION
 async function getTodos(){
     // try{
     //     // bất đồng bộ
@@ -28,9 +31,8 @@ async function getTodos(){
         // await đợi đến khi nào xử lí xong rồi mới chạy tiếp
         // await luôn luôn đi với async
         const response = await axios.get("https://68287be46b7628c529137a9e.mockapi.io/tasks/tasks");
-        console.log(response.data);
         const ul = document.querySelector(".todo-list");
-
+        // ul.innerHTML = "";
         response.data.forEach((element) => {
 
             const date = new Date(element.createdAt);
@@ -68,4 +70,61 @@ async function getTodos(){
     
 
     
+}
+
+
+
+// POST FUNCTION
+async function addTodo(){
+    const inputData = todoInput.value.trim();
+
+    const newTodo = {
+        createdAt: new Date().toISOString(),
+        content: inputData,
+        isComplete: false,
+    };
+
+    try {
+        const response = await axios.post("https://68287be46b7628c529137a9e.mockapi.io/tasks/tasks", newTodo);
+        todoInput.value = "";
+        getTodos();
+        Swal.fire({
+            title: "Adding successfully!!",
+            width: 600,
+            padding: "3em",
+            color: "#716add",
+            background: "#fff url(https://sweetalert2.github.io/images/trees.png)",
+            backdrop: 
+            `
+                rgba(0,0,123,0.4)
+                url("https://sweetalert2.github.io/images/nyan-cat.gif")
+                left top
+                no-repeat
+            `
+        });
+    } catch (error) {
+        console.log("ERROR EXCEPTION" + error);
+    }
+    
+}
+
+// PUT FUNCTION
+function handleUpdate(id, content){
+    Swal.fire({
+        title: "Edit your content",
+        input: "text",
+        inputAttributes: {
+        autocapitalize: "off"
+        },
+        inputValue: content,
+        showCancelButton: true,
+        confirmButtonText: "Save",
+        showLoaderOnConfirm: true,
+        preConfirm: async (dataInput) => {
+            await axios.put(`https://68287be46b7628c529137a9e.mockapi.io/tasks/tasks/${id}`,{
+                content: dataInput,
+            });
+        },
+    });
+
 }
