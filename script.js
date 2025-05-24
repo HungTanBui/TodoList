29275e42de3fcfaa489326f4b1e45cd49962d082
HungTanBui <staboyvn12@gmail.com>
2025-05-24 10:11:17 +0700

    Update HTML, CSS, JavaScript

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
+const API_URL = "https://68287be46b7628c529137a9e.mockapi.io/tasks/tasks";
 const todoInput = document.getElementById("todo-input");
 const addButton = document.getElementById("add-button");
 
+todoInput.addEventListener("keypress", (Event) => {
+  if (Event.key == "Enter") {
+    addTodo();
+  }
+});
 document.addEventListener("DOMContentLoaded", getTodos());
-addButton.addEventListener("click",addTodo);
+addButton.addEventListener("click", addTodo);
 // function getTodos(){
 
 //     // Lấy dữ liệu từ API thông qua fetch
@@ -17,32 +23,35 @@ addButton.addEventListener("click",addTodo);
 
 // Hàm bất đồng bộ
 // GET FUNCTION
-async function getTodos(){
-    // try{
-    //     // bất đồng bộ
-    //     const response = axios.get("https://68287be46b7628c529137a9e.mockapi.io/tasks/tasks")
-    //     .then((response) => console.log(response.data));
-    // }catch (Error){
-    //     console.log(Error);
-    // }
-    
+async function getTodos() {
+  // try{
+  //     // bất đồng bộ
+  //     const response = axios.get("https://68287be46b7628c529137a9e.mockapi.io/tasks/tasks")
+  //     .then((response) => console.log(response.data));
+  // }catch (Error){
+  //     console.log(Error);
+  // }
 
-    try{
-        // await đợi đến khi nào xử lí xong rồi mới chạy tiếp
-        // await luôn luôn đi với async
-        const response = await axios.get("https://68287be46b7628c529137a9e.mockapi.io/tasks/tasks");
-        const ul = document.querySelector(".todo-list");
-        // ul.innerHTML = "";
-        response.data.forEach((element) => {
-
-            const date = new Date(element.createdAt);
-            const formatedDate = `${date.toLocaleDateString()}-${date.toLocaleTimeString()} `;
-            console.log(formatedDate);
+  try {
+    // await đợi đến khi nào xử lí xong rồi mới chạy tiếp
+    // await luôn luôn đi với async
+    const response = await axios.get(API_URL);
+    const ul = document.querySelector(".todo-list");
+    // reset thẻ ul
+    ul.innerHTML = "";
+    // sắp xếp theo ngày mới nhất
+    response.data.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
 
 
-            const li = document.createElement("li");
-            li.className = "todo-item"
-            li.innerHTML = `
+    // tạo ra thẻ li mới trong ul
+    response.data.forEach((element) => {
+      const date = new Date(element.createdAt);
+      const formatedDate = `${date.toLocaleDateString()}-${date.toLocaleTimeString()} `;
+      console.log(formatedDate);
+
+      const li = document.createElement("li");
+      li.className = "todo-item";
+      li.innerHTML = `
                 <div class="todo-content">
 
                     <input type="checkbox">
@@ -54,77 +63,110 @@ async function getTodos(){
                         
                 </div>
                 <div class="todo-actions">
-                    <button><i class="fa-solid fa-pen-to-square"></i></button>
-                    <button><i class="fa-solid fa-trash-can"></i></button>
+                    <button onclick="handleUpdate(${element.id}, '${element.content}')"><i class="fa-solid fa-pen-to-square"></i></button>
+                    <button onclick="handleDelete(${element.id})"><i class="fa-solid fa-trash-can"></i></button>
                 </div>
-            `,
-            ul.appendChild(li)
-
-        });
-        
-        
-        
-    }catch (Error){
-        console.log(Error);
-    }
-    
-
-    
+            `;
+      ul.appendChild(li);
+    });
+  } catch (Error) {
+    console.log(Error);
+  }
 }
 
-
-
 // POST FUNCTION
-async function addTodo(){
-    const inputData = todoInput.value.trim();
+async function addTodo() {
+  // Input data
+  const inputData = todoInput.value.trim();
 
-    const newTodo = {
-        createdAt: new Date().toISOString(),
-        content: inputData,
-        isComplete: false,
-    };
+  // thuộc tính của data trong khung
+  const newTodo = {
+    createdAt: new Date().toISOString(),
+    content: inputData,
+    isComplete: false,
+  };
 
-    try {
-        const response = await axios.post("https://68287be46b7628c529137a9e.mockapi.io/tasks/tasks", newTodo);
-        todoInput.value = "";
-        getTodos();
-        Swal.fire({
-            title: "Adding successfully!!",
-            width: 600,
-            padding: "3em",
-            color: "#716add",
-            background: "#fff url(https://sweetalert2.github.io/images/trees.png)",
-            backdrop: 
-            `
-                rgba(0,0,123,0.4)
-                url("https://sweetalert2.github.io/images/nyan-cat.gif")
-                left top
-                no-repeat
-            `
-        });
-    } catch (error) {
-        console.log("ERROR EXCEPTION" + error);
-    }
-    
+  try {
+    const response = await axios.post(API_URL, newTodo);
+    todoInput.value = "";
+    getTodos();
+    showNotification("Added successfully!!!");
+  } catch (error) {
+    console.log("ERROR EXCEPTION" + error);
+  }
 }
 
 // PUT FUNCTION
-function handleUpdate(id, content){
-    Swal.fire({
-        title: "Edit your content",
-        input: "text",
-        inputAttributes: {
-        autocapitalize: "off"
-        },
-        inputValue: content,
-        showCancelButton: true,
-        confirmButtonText: "Save",
-        showLoaderOnConfirm: true,
-        preConfirm: async (dataInput) => {
-            await axios.put(`https://68287be46b7628c529137a9e.mockapi.io/tasks/tasks/${id}`,{
-                content: dataInput,
-            });
-        },
-    });
+function handleUpdate(id, content) {
+  Swal.fire({
+    title: "Edit your content",
+    input: "text",
+    inputAttributes: {
+      autocapitalize: "off",
+    },
+    inputValue: content,
+    showCancelButton: true,
+    confirmButtonText: "Save",
+    showLoaderOnConfirm: true,
+    preConfirm: async (dataInput) => {
+      await axios.put(`${API_URL}/${id}`, {
+        content: dataInput,
+      });
+      getTodos();
+      showNotification("Updated successfully!!!");
+    },
+  });
+}
 
+// DELETED FUNCTION
+function handleDelete(id) {
+  const swalWithBootstrapButtons = Swal.mixin({
+    customClass: {
+      confirmButton: "btn btn-success",
+      cancelButton: "btn btn-danger mr-5",
+    },
+    buttonsStyling: false,
+  });
+  swalWithBootstrapButtons
+    .fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Yes, delete it!",
+      cancelButtonText: "No, cancel!",
+      reverseButtons: true,
+    })
+    .then(async (result) => {
+      if (result.isConfirmed) {
+        await axios.delete(`${API_URL}/${id}`);
+        showNotification("Deleted successfully!!");
+        getTodos();
+      } else if (
+        /* Read more about handling dismissals below */
+        result.dismiss === Swal.DismissReason.cancel
+      ) {
+        swalWithBootstrapButtons.fire({
+          title: "Cancelled",
+          text: "Your imaginary file is safe :)",
+          icon: "error",
+        });
+      }
+    });
+}
+
+function showNotification(message) {
+  Swal.fire({
+    title: message,
+    width: 600,
+    padding: "3em",
+    color: "#716add",
+    background: "#fff url(https://sweetalert2.github.io/images/trees.png)",
+    backdrop: `
+            rgba(0,0,123,0.4)
+            url("https://sweetalert2.github.io/images/nyan-cat.gif")
+            left top
+            no-repeat
+        `,
+  });
 }
